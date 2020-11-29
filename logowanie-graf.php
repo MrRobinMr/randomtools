10cf6d7e26b415dd05984c178d0c100588d48e22
kurczakGeralt <75227191+kurczakGeralt@users.noreply.github.com>
2020-11-30 00:53:58 +0100

    wyglad

---
+++ logowanie-graf.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=opera,chrome=1" />
+  <title>Random tools</title>
+  <link rel="stylesheet" href="st.css">
+</head>
+<body>
+  <div class="baner">
+    <h1>Logowanie</h1>
+  </div>
+ <div class="srodek">
+  <form action="zaloguj.php" method="post">
+    Login<br /><br /><input type="text" name="login" placeholder="Login" /><br /><br /><br />
+    Hasło<br /><br /><input type="password" name="haslo" placeholder="Hasło" /><br /><br />
+    <input type="submit" value="Zaloguj się" />
+  </form>
+  <br /> <br />
+  <a href="rejestracja.php">Rejestracja</a>
+ </div>
+ <footer>
+   Najs
+ </footer>
+</body>
+</html>
---
+++ rejestracja-graf.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=opera,chrome=1" />
+	<title>Random tools</title>
+	<link rel="stylesheet" href="st.css">
+</head>
+<body>
+	<div class="baner">
+		<h1>Rejestracja</h1>
+	</div>
+	<div class="srodek">
+	<form method="post" >
+		Nickname <br /><br /> <input type="text" name="nick" /><br /><br /><br />
+		Imie <br /><br /> <input type="text" name="imie" /><br /><br /><br />
+		Nazwisko <br /><br /> <input type="text" name="nazwisko" /><br /><br /><br />
+		Numer telefonu <br /><br /> <input type="number" name="numer" /><br /><br /><br />
+		Adres <br /><br /> <input type="text" name="adres" /><br /><br /><br />
+		Poczta <br /><br /> <input type="text" name="poczta" /><br /><br /><br />
+		E-mail <br /><br /> <input type="text" name="email" /><br /><br /><br />
+		Twoje hasło <br /><br /> <input type="password" name="haslo1" /><br /><br /><br />
+		Powtórz hasło <br /><br /> <input type="password" name="haslo2" /><br />
+		<br /><br />
+		<label>
+			<input type="checkbox" name="regulamin" />Akceptuję regulamin
+		</label>
+		<br /><br />
+		<input type="submit" value="Zarejestruj się" />
+	</form>
+	<br /> <br />
+  <a href="index.php">Logowanie</a>
+	</div>
+	<footer>
+    Najs
+  </footer>
+</body>
+</html>
