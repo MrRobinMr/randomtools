9455eb0db7e29e7c6e34567c23f06209887c8d71
MrRobinMr <44535304+MrRobinMr@users.noreply.github.com>
2020-12-04 15:32:13 +0100

    Update rejestracja-graf.php

--- rejestracja-graf.php
+++ rejestracja-graf.php
@@ -1,5 +1,11 @@
 <?php
-
+require_once "conf.php";
+try{
+ $db = new PDO("mysql:host=".$host.";dbname=".$db_name, $db_user, $db_password);
+}
+catch (PDOException $e){
+ die ("Error connecting to database!");
+}
 if(isset($_POST['rejstracja']))
 {
  $email = $_POST['email'];
@@ -9,7 +15,7 @@ if(isset($_POST['rejstracja']))
  $nazwisko = $_POST['nazwisko'];
  if($password==$chpass){
  $hashPassword = password_hash($password,PASSWORD_BCRYPT);
- $sth = $db->prepare('INSERT INTO user (imie,nazwisko,haslo,email) VALUE
+ $sth = $db->prepare('INSERT INTO klienci (imie,nazwisko,haslo,email) VALUE
 (:imie,:nazwisko,:password,:email)');
 $sth->bindValue(':imie', $imie, PDO::PARAM_STR);
 $sth->bindValue(':nazwisko', $nazwisko, PDO::PARAM_STR);
