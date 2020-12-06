0feec403a7b8793d9f9169a7754cbe4bc277c716
Zomyr <g.skiermunt@gmail.com>
2020-12-06 23:20:07 +0100

    regulamin

---
+++ regulamin.php
@@ -0,0 +1,85 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=opera,chrome=1" />
+	<title>Random tools</title>
+	<link rel="stylesheet" href="st.css">
+	<link rel="icon" href="logo1-ConvertImage.ico" type="image"/>
+</head>
+<body>
+	<?php
+		include_once('header1.php');
+	?>
+
+
+   
+   <p>1.Postanowienia ogólne</p>
+   <p>2.Definicje</p>
+   <p>3.Rodzaj i zakres usług elektronicznych</p>
+   <p>4.Warunki świadczenia i zawierania umów o świadczenie usług elektronicznych </p>
+   <p>5.Warunki rozwiązania umów o świadczenie usług elektronicznych </p>
+   <p>6.Tryb postępowania reklamacyjnego </p>
+   <p>7.Własność intelektualna </p>
+   <p>8.Postanowienia końcowe </p>
+   <p>1.POSTANOWIENIA OGÓLNE </p>
+    <p style="text-align:justify"> Strona Random tools działa na zasadach określonych w niniejszym Regulaminie.
+    Regulamin określa rodzaje i zakres usług świadczonych drogą elektroniczną przez Stronę Random tools, zasady świadczenia tych usług, warunki zawierania i rozwiązywania umów o świadczenie usług drogą elektroniczną, a także tryb postępowania reklamacyjnego.
+    Regulamin określa rodzaje i zakres usług świadczonych drogą elektroniczną przez Stronę Random tools, zasady świadczenia tych usług, warunki zawierania i rozwiązywania umów o świadczenie usług drogą elektroniczną, a także tryb postępowania reklamacyjnego.
+    Każdy Usługobiorca z chwilą podjęcia czynności zmierzających do korzystania z Usług Elektronicznych Strony Random tools, zobowiązany jest do przestrzegania postanowień niniejszego Regulaminu. 
+    W sprawach nieuregulowanych w niniejszym Regulaminie mają zastosowanie przepisy
+    Ustawy o świadczeniu usług drogą elektroniczną z dnia 18 lipca 2002 r. (Dz. U. Nr 144, poz. 1204 ze zm.),
+    Ustawy o prawach konsumenta z dnia 30 maja 2014 r. (Dz. U. 2014 poz. 827),
+    Ustawy Kodeks cywilny z dnia 23 kwietnia 1964 r. (Dz. U. nr 16, poz. 93 ze zm.) oraz inne właściwe przepisy prawa polskiego.</p>
+   <p style="text-align:justify">2.DEFINICJE </p>
+    <p style="text-align:justify">FORMULARZ KONTAKTOWY – formularz dostępny na Stronie Random tools umożliwiający Usługobiorcy bezpośredni kontakt z Usługodawcą.
+    REGULAMIN - niniejszy regulamin Strony.
+    USŁUGODAWCA – Random tools, adres siedziby: Siedlce, adres do doręczeń: Siedlce wpisaną do rejestru przedsiębiorców pod numerem KRS: 0000503432, NIP: 3592441266, REGON: 128347592, adres poczty elektroniczne: sklep@randomtools.pl, tel.: 134 312 452.
+    USŁUGOBIORCA – osoba fizyczna, osoba prawna albo jednostka organizacyjna nieposiadająca osobowości prawnej, której ustawa przyznaje zdolność prawną korzystająca z Usługi Elektronicznej.
+    USŁUGA ELEKTRONICZNA – usługa świadczona drogą elektroniczną przez Usługodawcę na rzecz Usługobiorcy za pośrednictwem Strony.
+    NEWSLETTER – Usługa Elektroniczna pozwalająca Usługobiorcy na subskrybowanie i otrzymywanie na podany przez Usługobiorcę adres e-mail bezpłatnych informacji pochodzących od Usługodawcy.</p>
+    <p style="text-align:justify">3.RODZAJ I ZAKRES USŁUG ELEKTRONICZNYCH</p>
+    <p style="text-align:justify">Usługodawca umożliwia za pośrednictwem Strony korzystanie z Usług Elektronicznych takich jak:
+    korzystanie z Formularza Kontaktowego,
+    korzystanie z Newslettera,
+    Świadczenie Usług Elektronicznych na rzecz Usługobiorców odbywa się na warunkach określonych w Regulaminie.
+    <p style="text-align:justify">4.WARUNKI ŚWIADCZENIA I ZAWIERANIA UMÓW O ŚWIADCZENIE USŁUG ELEKTRONICZNYCH</p>
+    Świadczenie Usług Elektronicznych określonych w rozdziale III pkt. 1 Regulaminu przez Usługodawcę jest nieodpłatne.
+    Okres na jaki umowa zostaje zawarta:
+    umowa o świadczenie Usługi Elektronicznej polegającej na umożliwieniu wysłania wiadomości za pośrednictwem Formularza Kontaktowego zawierana jest na czas oznaczony i ulega rozwiązaniu z chwilą wysłania wiadomości albo zaprzestania jej wysyłania przez Usługobiorcę.
+    umowa o świadczenie Usługi Elektronicznej polegającej na korzystaniu z Newslettera zawierana jest na czas nieoznaczony.
+    Wymagania techniczne niezbędne do współpracy z systemem teleinformatycznym, którym posługuje się Usługodawca:
+    komputer z dostępem do Internetu,
+    dostęp do poczty elektronicznej,
+    przeglądarka internetowa,
+    włączenie w przeglądarce internetowej Cookies oraz Javascript.
+    Usługobiorca zobowiązany jest do korzystania ze Strony w sposób zgodny z prawem i dobrymi obyczajami mając na uwadze poszanowanie dóbr osobistych i praw własności intelektualnej osób trzecich.
+    Usługobiorca zobowiązany jest do wprowadzania danych zgodnych ze stanem faktycznym.
+    Usługobiorcę obowiązuje zakaz dostarczania treści o charakterze bezprawnym.</p>
+    <p style="text-align:justify">5.WARUNKI ROZWIĄZYWANIA UMÓW O ŚWIADCZENIE USŁUG ELEKTRONICZNYCH</p>
+    <p style="text-align:justify">Wypowiedzenie umowy o świadczenie Usługi Elektronicznej:
+    Wypowiedzeniu może ulec umowa o świadczenie Usługi Elektronicznej o charakterze ciągłym i bezterminowym (np. korzystanie z Newslettera).
+    Usługobiorca może wypowiedzieć umowę ze skutkiem natychmiastowym i bez wskazywania przyczyn poprzez przesłanie stosownego oświadczenia za pośrednictwem poczty elektronicznej na adres: sklep@randomtools.pl
+    Usługodawca może wypowiedzieć umowę o świadczenie Usługi Elektronicznej o charakterze ciągłym i bezterminowym w przypadku, gdy Usługobiorca narusza Regulamin, w szczególności, gdy dostarcza treści o charakterze bezprawnym po bezskutecznym wcześniejszym wezwaniu do zaprzestania naruszeń z wyznaczeniem odpowiedniego terminu. Umowa w takim wypadku wygasa po upływie 7 dni od dnia złożenia oświadczenia woli o jej wypowiedzeniu (okres wypowiedzenia).
+    Wypowiedzenie prowadzi do ustania stosunku prawnego ze skutkiem na przyszłość.
+    Usługodawca i Usługobiorca mogą rozwiązać umowę o świadczenie Usługi Elektronicznej w każdym czasie w drodze porozumienia stron.</p>
+    <p style="text-align:justify">6.TRYB POSTĘPOWANIA REKLAMACYJNEGO</p>
+    <p style="text-align:justify">Reklamacje związane ze świadczeniem Usług Elektronicznych przez Usługodawcę:
+    Reklamacje związane ze świadczeniem Usług Elektronicznych za pośrednictwem Strony Usługobiorca może składać za pośrednictwem poczty elektronicznej na adres: sklep@randomtools.pl
+    W powyższej wiadomości e-mail, należy podać jak najwięcej informacji i okoliczności dotyczących przedmiotu reklamacji, w szczególności rodzaj i datę wystąpienia nieprawidłowości oraz dane kontaktowe. Podane informacje znacznie ułatwią i przyspieszą rozpatrzenie reklamacji przez Usługodawcę.
+    Rozpatrzenie reklamacji przez Usługodawcę następuje niezwłocznie, nie później niż w terminie 14 dni.
+    Odpowiedź Usługodawcy w sprawie reklamacji jest wysyłana na adres e-mail Usługobiorcy podany w zgłoszeniu reklamacyjnym lub w inny podany przez Usługobiorcę sposób.
+    <p style="text-align:justify">7.WŁASNOŚĆ INTELEKTUALNA</p>
+    <p style="text-align:justify">Wszystkie treści zamieszczone na stronie internetowej pod adresem Random tools korzystają z ochrony prawno autorskiej i są własnością Random tools Usługobiorca ponosi pełną odpowiedzialność za szkodę wyrządzoną Usługodawcy, będącą następstwem użycia jakiejkolwiek zawartości strony Random tools, bez zgody Usługodawcy.
+    Jakiekolwiek wykorzystanie przez kogokolwiek, bez wyraźnej pisemnej zgody Usługodawcy, któregokolwiek z elementów składających się na treść oraz zawartość strony Random tools stanowi naruszenie prawa autorskiego przysługującego Usługodawcy i skutkuje odpowiedzialnością cywilnoprawną oraz karną.
+    <p style="text-align:justify">8.POSTANOWIENIA KOŃCOWE</p>
+    Umowy zawierane za pośrednictwem Strony zawierane są zgodnie z prawem polskim.
+    W przypadku niezgodności jakiejkolwiek części Regulaminu z obowiązującym prawem, w miejsce zakwestionowanego przepisu Regulaminu zastosowanie mają właściwe przepisy prawa polskiego.
+
+
+	<?php
+		include_once('footer.php');
+	?>
+
+</body>
+</html>
