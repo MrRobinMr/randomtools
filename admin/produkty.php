cb795cce50d377d47eec5c6e2f479b02775302a4
MrRobinMr <44535304+MrRobinMr@users.noreply.github.com>
2020-12-06 22:48:54 +0100

    Praca przy produktach

--- admin/produkty.php
+++ admin/produkty.php
@@ -4,11 +4,13 @@ echo "<link rel=\"stylesheet\" href=\"a_style.css\">";
 $sth = $db->prepare('SELECT ID_produkty,nazwa,nazwakat,cena,na_stanie,VAT,nazwapro FROM produkty natural join kategorie natural join producent');
 $sth->execute();
 echo "<h1>Produkty<h1>";
+echo "<form action=\"pro_dodaj.php\" method=\"post\"><button type=\"submit\" name=\"edytuj\">Dodaj</button></form>";
 echo "<table>";
 while($user = $sth->fetch(PDO::FETCH_ASSOC)){
   $vat = (double) $user["VAT"]*100;
   echo "<tr><td>".$user["nazwapro"]."</td><td>".$user["nazwa"]."</td><td>"
-  .$user["nazwakat"]."</td><td>".$user["cena"]." ZŁ</td><td>".$vat." %</td><td>
+  .$user["nazwakat"]."</td><td>"
+  .$user["na_stanie"]."</td><td>".$user["cena"]." ZŁ</td><td>".$vat." %</td><td>
   <form action=\"user_edycja.php\" method=\"post\"><input type=\"hidden\" name=\"id\" value=\""
 .$user["ID_produkty"]."\">
   <button type=\"submit\" name=\"edytuj\">Edytuj</button>
