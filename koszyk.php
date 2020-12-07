f751af684410b6d6296f33d37c8b081e533c2503
kurczakGeralt <75227191+kurczakGeralt@users.noreply.github.com>
2020-12-07 14:49:34 +0100

    otwarcie sesji w koszyku

--- koszyk.php
+++ koszyk.php
@@ -1,20 +1,5 @@
-<!DOCTYPE HTML>
-<html lang="pl">
-<head>
-	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=opera,chrome=1" />
-	<title>Random tools</title>
-	<link rel="stylesheet" href="st.css">
-	<link rel="icon" href="logo1-ConvertImage.ico" type="image"/>
-</head>
-<body>
-	<?php
-		include_once('header1.php');
-	?>
+<?php
 
-  <?php
-		include_once('footer.php');
-	?>
+session_start();
 
-</body>
-</html>
+?>
