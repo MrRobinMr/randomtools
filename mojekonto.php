cbce607508295ce0237798410d70e0da58914530
Zomyr <g.skiermunt@gmail.com>
2020-12-07 21:09:35 +0100

    Create mojekonto.php

---
+++ mojekonto.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=opera,chrome=1" />
+	<title>Random tools</title>
+	<link rel="stylesheet" href="st.css">
+	<link rel="icon" href="logo1-ConvertImage.ico" type="image"/>
+</head>
+<body>
+	<?php
+		include_once('header1.php');
+	?>
+
+                        
+        
+
+
+
+<?php
+		include_once('footer.php');
+	?>
+
+</body>
+</html>
