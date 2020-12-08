301dc6c6ebaadcaaecfee260064d9332d540b049
MrRobinMr <44535304+MrRobinMr@users.noreply.github.com>
2020-12-08 01:54:34 +0100

    nie wiem co zrobić pls help

--- zam.php
+++ zam.php
@@ -1,7 +1,19 @@
 <?php
 session_start();
-foreach ($_SESSION["koszyk"] as $key => $value) {
-  echo $_POST[$value];
+$cena = 0;
+require_once "conf.php";
+try{
+ $db = new PDO("mysql:host=".$host.";dbname=".$db_name, $db_user, $db_password);
 }
-
+catch (PDOException $e){
+ die ("Error connecting to database!");
+}
+foreach ($_SESSION["koszyk"] as $value) {
+  $sth = $db->prepare('SELECT cena from produkty where id=:id');
+  $sth->bindValue(':id', $value, PDO::PARAM_STR);
+  $sth->execute();
+  $c = $sth->fetch();
+  echo $c;
+}
+echo $cena;
  ?>
